c19cf25cf8a9fa76b4c0be4be219418395eb9f21
2968882074 <2968882074@.qq.com>
2020-12-25 15:03:48 +0800

    第三次更改

---
+++ src/com/yidu/Like.java
@@ -0,0 +1,13 @@
+package com.yidu;
+
+/**
+ * @author yjm
+ * @version 1.0
+ * @description:
+ * @date 2020/12/25 15:03
+ */
+public class Like {
+    public static void main(String[] args) {
+        System.out.println("明天会更好！");
+    }
+}
